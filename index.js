33acbe20d3000fac9acfbd112c11cf50ec577b65
David <david.owuor@student.moringaschool.com>
2022-11-26 18:21:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const arrayNames = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+function writeCards(arrayNames){
+  for(let counter = 0; counter < arrayNames.length; counter = counter+1){
+    console.log(messages.push(`Thank you, ${arrayNames[counter]}, for the wonderful surprise gift!`))
+  }
+  return messages;
+}
+
+function countDown(){
+    let number = 10;
+    while(number > -1){
+        console.log(number);  
+        number = number-1;
+    }
+}
